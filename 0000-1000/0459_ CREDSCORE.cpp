3d01da80bcf7a09c0df2d7e22b140b6779b14788
Osvaldo Silva <osvaldo2001@hotmail.com>
2022-04-10 00:34:13 +0200

    Create 0459_ CREDSCORE.cpp

---
+++ 0000-1000/0459_ CREDSCORE.cpp
@@ -0,0 +1,11 @@
+// https://www.codechef.com/problems/CREDSCORE
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin >> x;
+	x >= 750 ? cout << "YES\n" : cout << "NO\n";
+	return 0;
+}
